93064362867f20a5df54757929f9b327af800ac5
GitMarieM <mama21ki@student.ju.se>
2023-09-01 10:44:02 +0200

    Add first experiment

---
+++ experiments/marie-experiment1.js
@@ -0,0 +1,70 @@
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(34, 39, 46);
+}
+
+function draw() {
+  background(252, 186, 3, 40);
+  noStroke();
+  fill(46, 14, 32, 70);
+
+  push();
+  translate(width / 2, height / 2);
+
+  push();
+  rotate(frameCount / 8);
+  circle(25, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(-frameCount / 10);
+  circle(50, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(frameCount / 9);
+  circle(75, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(frameCount / 4);
+  circle(25, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(-frameCount / 5);
+  circle(50, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(frameCount / 10);
+  circle(75, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(-frameCount / 6);
+  circle(100, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(frameCount / 4);
+  circle(35, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(-frameCount / 5);
+  circle(45, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(frameCount / 7);
+  circle(55, 0, 30, 50);
+  pop();
+
+  push();
+  rotate(-frameCount / 8);
+  circle(65, 0, 30, 50);
+  pop();
+
+  pop();
+}
